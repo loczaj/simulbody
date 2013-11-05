a9d54943e081f7ef2c1c9d9f9e8db1ae1c52c2d7
Roland Lohner <loczaj@gmail.com>
2013-11-05 14:28:29 +0100

    More build targets in Jamroot

--- simulator/test/revolution.cpp
+++ simulator/test/revolution.cpp
@@ -34,12 +34,14 @@ int main(int argc, char* atgv[]) {
 	iss = bbsystem.createBody(0.001, vector3D(0.2, 0.0, 0.0), vector3D(0.0, 3.5, 0.0));
 	apollo = bbsystem.createBody(0.001, vector3D(1.1, 0.0, 0.0), vector3D(0.0, 3.5, 0.0));
 
-	Interaction* gravityEM = new GravitationalInteraction(5, earth, moon);
-	Interaction* gravityEI = new GravitationalInteraction(0.005, earth, iss);
-	Interaction* gravityEA = new GravitationalInteraction(0.005, earth, apollo);
-	Interaction* gravityMA = new GravitationalInteraction(0.001, moon, apollo);
-	Interaction* gravityMI = new GravitationalInteraction(0.0001, moon, iss);
-	Interaction* gravityIA = new GravitationalInteraction(0.000001, iss, apollo);
+	bbsystem.convertToCenterOfMassSystem();
+
+	Interaction* gravityEM = new GravitationalInteraction(1, earth, moon);
+	Interaction* gravityEI = new GravitationalInteraction(0.001, earth, iss);
+	Interaction* gravityEA = new GravitationalInteraction(0.001, earth, apollo);
+	Interaction* gravityMA = new GravitationalInteraction(0.0002, moon, apollo);
+	Interaction* gravityMI = new GravitationalInteraction(0.00002, moon, iss);
+	Interaction* gravityIA = new GravitationalInteraction(0.0000002, iss, apollo);
 
 	bbsystem << gravityEM << gravityEI << gravityEA << gravityMA << gravityMI << gravityIA;
 
@@ -47,7 +49,7 @@ int main(int argc, char* atgv[]) {
 	std::cout << "E0=" << bbsystem.getSystemEnergy() << std::endl;
 
 	runge_kutta4_classic<Phase, double, Phase, double, range_algebra> stepper;
-	int steps = integrate_const(stepper, rhs, bbsystem.phase, 0.0, 2.4, 0.001, write_state());
+	int steps = integrate_const(stepper, rhs, bbsystem.phase, 0.0, 10.0, 0.001, write_state());
 
 	stream.close();
 	std::cout << "En=" << bbsystem.getSystemEnergy() << std::endl;
