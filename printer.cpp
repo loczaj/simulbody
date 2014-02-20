3108fa369befca574d1c09450fc9325c3466e431
Roland Lohner <loczaj@gmail.com>
2014-02-20 17:20:31 +0100

    Move printer implementation to source file

--- printer.cpp
+++ printer.cpp
@@ -2,6 +2,7 @@
 
 namespace simulbody {
 
+// *** Printer *** //
 Printer::Printer(std::string fileName) {
 	fileStream = new std::ofstream(fileName, std::ofstream::out);
 	stream = fileStream;
@@ -42,6 +43,115 @@ Printer::~Printer() {
 		delete fileStream;
 		fileStream = nullptr;
 	}
+}
+
+// *** BodyPrintField ***
+BodyPrintField::BodyPrintField(identifier body, std::initializer_list<Coord> coords)
+		: body(body) {
+	for (Coord c : coords) {
+		coordinates.push_back(c);
+	}
+}
+
+void BodyPrintField::writeField(const Phase &phase, const double &time, std::ostream &stream) {
+	for (sizeT i = 0; i < coordinates.size(); i++) {
+
+		switch (coordinates[i]) {
+		case Coord::x:
+			stream << phase.getBodyPosition(body).x;
+			break;
+		case Coord::y:
+			stream << phase.getBodyPosition(body).y;
+			break;
+		case Coord::z:
+			stream << phase.getBodyPosition(body).z;
+			break;
+		case Coord::vx:
+			stream << phase.getBodyVelocity(body).x;
+			break;
+		case Coord::vy:
+			stream << phase.getBodyVelocity(body).y;
+			break;
+		case Coord::vz:
+			stream << phase.getBodyVelocity(body).z;
+		}
+
+		if (i < coordinates.size() - 1)
+			stream << "\t";
+	}
+}
+
+// *** PositionPrintField ***
+void PositionPrintField::writeField(const Phase &phase, const double &time, std::ostream &stream) {
+	for (identifier body = 0; body < phase.getNumberOfBodies(); body++) {
+
+		vector3D r = phase.getBodyPosition(body);
+		stream << r.x << "\t" << r.y << "\t" << r.z;
+
+		if (body < phase.getNumberOfBodies() - 1)
+			stream << "\t";
+	}
+}
+
+// *** TimePrintField ***
+void TimePrintField::writeField(const Phase &phase, const double &time, std::ostream &stream) {
+	stream << time;
+}
+
+// *** DistancePrintField ***
+DistancePrintField::DistancePrintField(identifier earth, identifier moon)
+		: earth(earth), moon(moon) {
+}
+
+void DistancePrintField::writeField(const Phase &phase, const double &time, std::ostream &stream) {
+	stream << phase.getBodyPosition(earth).distance(phase.getBodyPosition(moon));
+}
+
+// *** ConditionPrintField ***
+ConditionPrintField::ConditionPrintField(Condition* condition)
+		: condition(condition) {
+}
+
+void ConditionPrintField::writeField(const Phase &phase, const double &time, std::ostream &stream) {
+	if (condition->evaluate(phase, time)) {
+		stream << "1";
+	} else {
+		stream << "0";
+	}
+}
+
+// *** InteractionPrintField ***
+InteractionPrintField::InteractionPrintField(Interaction* interaction, InteractionAttribute attribute)
+		: interaction(interaction), attribute(attribute) {
+}
+
+void InteractionPrintField::writeField(const Phase &phase, const double &time, std::ostream &stream) {
+	switch (attribute) {
+	case InteractionAttribute::relativePosition:
+		stream << interaction->relativePosition;
+		break;
+	case InteractionAttribute::relativeVelocity:
+		stream << interaction->relativeVelocity;
+		break;
+	case InteractionAttribute::energy:
+		stream << interaction->getEnergy(phase);
+		break;
+	case InteractionAttribute::actingForce:
+		stream << interaction->actingForce;
+		break;
+	case InteractionAttribute::actingVelocity:
+		stream << interaction->actingVelocity;
+		break;
+	}
+}
+
+// *** CustomPrintField ***
+CustomPrintField::CustomPrintField(boost::function<void(const Phase, const double, std::ostream&)> writer)
+		: writer(writer) {
+}
+
+void CustomPrintField::writeField(const Phase &phase, const double &time, std::ostream &stream) {
+	writer(phase, time, stream);
 }
 
 } /* NAMESPACE SIMULBODY */
--- printer.hpp
+++ printer.hpp
@@ -13,7 +13,6 @@
 
 namespace simulbody {
 
-// *** PrintField ***
 class PrintField {
 public:
 	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) = 0;
@@ -21,7 +20,6 @@ public:
 	}
 };
 
-// *** Printer ***
 class Printer {
 	std::vector<PrintField*> fields;
 	std::ostream* stream = nullptr;
@@ -39,105 +37,42 @@ public:
 	~Printer();
 };
 
-// *** BodyPrintField ***
 class BodyPrintField: public PrintField {
 	identifier body;
 	std::vector<Coord> coordinates;
 
 public:
-	BodyPrintField(identifier body, std::initializer_list<Coord> coords)
-			: body(body) {
-		for (Coord c : coords) {
-			coordinates.push_back(c);
-		}
-	}
-
-	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override {
-		for (sizeT i = 0; i < coordinates.size(); i++) {
-
-			switch (coordinates[i]) {
-			case Coord::x:
-				stream << phase.getBodyPosition(body).x;
-				break;
-			case Coord::y:
-				stream << phase.getBodyPosition(body).y;
-				break;
-			case Coord::z:
-				stream << phase.getBodyPosition(body).z;
-				break;
-			case Coord::vx:
-				stream << phase.getBodyVelocity(body).x;
-				break;
-			case Coord::vy:
-				stream << phase.getBodyVelocity(body).y;
-				break;
-			case Coord::vz:
-				stream << phase.getBodyVelocity(body).z;
-			}
-
-			if (i < coordinates.size() - 1)
-				stream << "\t";
-		}
-	}
+	BodyPrintField(identifier body, std::initializer_list<Coord> coords);
+	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override;
 };
 
-// *** PositionPrintField ***
 class PositionPrintField: public PrintField {
 public:
-	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override {
-		for (identifier body = 0; body < phase.getNumberOfBodies(); body++) {
-
-			vector3D r = phase.getBodyPosition(body);
-			stream << r.x << "\t" << r.y << "\t" << r.z;
-
-			if (body < phase.getNumberOfBodies() - 1)
-				stream << "\t";
-		}
-	}
+	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override;
 };
 
-// *** TimePrintField ***
 class TimePrintField: public PrintField {
 public:
-	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override {
-		stream << time;
-	}
+	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override;
 };
 
-// *** DistancePrintField ***
 class DistancePrintField: public PrintField {
 	identifier earth;
 	identifier moon;
 
 public:
-	DistancePrintField(identifier earth, identifier moon)
-			: earth(earth), moon(moon) {
-	}
-
-	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override {
-		stream << phase.getBodyPosition(earth).distance(phase.getBodyPosition(moon));
-	}
+	DistancePrintField(identifier earth, identifier moon);
+	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override;
 };
 
-// *** ConditionPrintField ***
 class ConditionPrintField: public PrintField {
 	Condition* condition;
 
 public:
-	ConditionPrintField(Condition* condition)
-			: condition(condition) {
-	}
-
-	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override {
-		if (condition->evaluate(phase, time)) {
-			stream << "1";
-		} else {
-			stream << "0";
-		}
-	}
+	ConditionPrintField(Condition* condition);
+	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override;
 };
 
-// *** InteractionPrintField ***
 enum class InteractionAttribute {
 	relativePosition, relativeVelocity, energy, actingForce, actingVelocity
 };
@@ -147,43 +82,16 @@ class InteractionPrintField: public PrintField {
 	InteractionAttribute attribute;
 
 public:
-	InteractionPrintField(Interaction* interaction, InteractionAttribute attribute)
-			: interaction(interaction), attribute(attribute) {
-	}
-
-	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override {
-		switch (attribute) {
-		case InteractionAttribute::relativePosition:
-			stream << interaction->relativePosition;
-			break;
-		case InteractionAttribute::relativeVelocity:
-			stream << interaction->relativeVelocity;
-			break;
-		case InteractionAttribute::energy:
-			stream << interaction->getEnergy(phase);
-			break;
-		case InteractionAttribute::actingForce:
-			stream << interaction->actingForce;
-			break;
-		case InteractionAttribute::actingVelocity:
-			stream << interaction->actingVelocity;
-			break;
-		}
-	}
+	InteractionPrintField(Interaction* interaction, InteractionAttribute attribute);
+	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override;
 };
 
-// *** CustomPrintField ***
 class CustomPrintField: public PrintField {
 	boost::function<void(const Phase, const double, std::ostream&)> writer;
 
 public:
-	CustomPrintField(boost::function<void(const Phase, const double, std::ostream&)> writer)
-			: writer(writer) {
-	}
-
-	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override {
-		writer(phase, time, stream);
-	}
+	CustomPrintField(boost::function<void(const Phase, const double, std::ostream&)> writer);
+	virtual void writeField(const Phase &phase, const double &time, std::ostream &stream) override;
 };
 
 } /* NAMESPACE SIMULBODY */
